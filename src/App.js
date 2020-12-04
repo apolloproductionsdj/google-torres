e0f4f82bdb3139f6377e5ae39389387967e47799
Mike Torres <apolloproductionsdj@gmail.com>
2020-12-03 22:47:25 -0700

    installed react router and set up links to pages for search, gmail, etc.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import Home from './pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="app">
-      <h1>Hey Clever Programmer Fam!</h1>
+      <Router>
+        <Switch>
+          <Route path="/search">
+            {/* <SearchPage /> */}
+            <h1>This is the search page</h1>
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router >
     </div>
+
+
+
   );
 }
 
