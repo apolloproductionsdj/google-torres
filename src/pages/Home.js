324f53a31cf1e2165b3fc930a1201afcf0caf12a
Mike Torres <apolloproductionsdj@gmail.com>
2020-12-06 17:56:28 -0700

    finished with <Header />

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Home.css';
 import { Link } from 'react-router-dom';
+import AppsIcon from '@material-ui/icons/Apps';
+import { Avatar } from '@material-ui/core';
 
 function Home() {
     return (
@@ -8,12 +10,13 @@ function Home() {
             <div className="home__header">
                 <div className="home__headerLeft">
                     <Link to="about">About</Link>
-                    <Link to="store">store</Link>
+                    <Link to="store">Store</Link>
                 </div>
                 <div className="home__headerRight">
                     <Link to="gmail">Gmail</Link>
                     <Link to="images">Images</Link>
-                    {/* Link */}
+                    <AppsIcon />
+                    <Avatar />
                     {/* Avatar */}
                 </div>
             </div>
